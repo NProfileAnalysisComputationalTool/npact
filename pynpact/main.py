7a6ebfa2db6111348dc0eb54e84761fb6a256762
Nathan Bird <nathan@acceleration.net>
2011-07-05 17:15:45 -0400

    Skeleton of calling the CG program from python, some refactoring to make determining whether we need a new file easier. re Programming:#555 (0.5)

--- pynpact/main.py
+++ pynpact/main.py
@@ -1,26 +1,57 @@
 import logging, os.path
 from Bio import SeqIO
 
+import util
 
+BINPATH="/home/ACCELERATION/nathan/projects/spat/luciano-c/bin"
+def binfile(name) :
+    os.path.join(BINPATH, name)
 
 class GenBankProcessor(object) :
     #Genbank files are documented at: http://www.ncbi.nlm.nih.gov/Sitemap/samplerecord.html
     gbkfile = None
-
+    force = False
+    logger = logging
 
     def __init__(self, gbkfile = None) :
         if gbkfile :
             self.parse(gbkfile)
+
+    def parse(self,gbkfile) :
+        self.gbkfile = gbkfile
+
+        if os.path.exists(gbkfile) :
+            self.logger.info("Parsing genbank file: %r", gbkfile)
+            #TODO: gbk can have multiple records in which case this
+            #will err (i wasn't able to find one that did though)
+            self.seqrec = SeqIO.read(gbkfile,"genbank")
+
+
+    def derivative_filename(self, part) :
+        """Build the filename of a derivative product of the gbk
+        file. If the derivative file already exists return whether it is out of date"""
+        if not part[0] == "." :
+            part = "." + part
+
+        outfilename = os.path.splitext(self.gbkfile)[0] + part
+
+        outofdate = self.force or (not os.path.exists(outfilename)) or \
+                    os.path.getmtime(outfilename) < os.path.getmtime(self.gbkfile)
+
+        return (outfilename, outofdate)
+
 
     def run_extract(self,gene_descriptor="gene") :
         """Go through the genbank record pulling out gene names and locations
 $ extract MYCGE.gbk 0 gene 0 locus_tag > MYCGE.genes
 """
 
-        #this can somewhat be done with the SeqIO.features list, but not quite the same.
 
+        outfilename,generate = self.derivative_filename(".extracted")
 
-        outfilename = os.path.splitext(self.gbkfile)[0] + ".extracted"
+        if not generate :
+            self.logger.debug("Skipped extraction")
+            return outfilename
 
         with open(outfilename,'w') as outfile :
             def print_feature(desc, strand, start, end) :
@@ -45,20 +76,19 @@ $ extract MYCGE.gbk 0 gene 0 locus_tag > MYCGE.genes
                                 ex += 1
                         else :
                             print_feature(desc[0],f.strand,f.location.nofuzzy_start, f.location.nofuzzy_end)
-
-
-    def parse(self,gbkfile) :
-        self.gbkfile = gbkfile
-        #TODO: gbk can have multiple records in which case this will
-        #err (i wasn't able to find one that did though)
-        self.seqrec = SeqIO.read(gbkfile,"genbank")
-
+        return outfilename
 
     def run_CG(self) :
         """Do the CG ratio calculations.
 $ CG MYCGE.gbk 1 580074 201 51 3 > MYCGE.CG200
 """
-        pass
+        outfilename,generate = self.derivative_filename("cg200ratio")
+        if generate :
+            with open(outfilename,'w') as outfile :
+                #TODO: Need to check this length
+                util.capturedCall([binfile("CG"), self.gbkfile, 1, len(self.seqreq), 201, 51, 3],
+                                  stdout=outfile,logger=self.logger)
+        return outfilename
 
     def run_atg(self) :
         # ../atg MYCGE.gbk > atg.txt
