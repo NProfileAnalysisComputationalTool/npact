198a4978035e87894c4fcf11e66cfdea67f54f35
Nathan Bird <nathan@acceleration.net>
2011-07-12 10:36:12 -0400

    fill-paragraph on license

--- pynpact/util.py
+++ pynpact/util.py
@@ -234,7 +234,9 @@ with mkstemp_overwrite('foobar.txt') as f:
 # Copright (c) 2011  Accelerated Data Works
 # All rights reserved.
 
-# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
 
 #     Redistributions of source code must retain the above copyright
 #     notice, this list of conditions and the following disclaimer.
