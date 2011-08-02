6b17cb810b373c67967c1333ad058e5097d55a8d
Nathan Bird <nathan@acceleration.net>
2011-08-02 12:04:10 -0400

    A little bit more work on getting virtualenvs and packaging sorted out re Programming:#637 (0.25)

---
+++ .dir-locals.el
@@ -0,0 +1,2 @@
+((nil . ((virtualenv-workon . "spat")
+	 (virtualenv-default-directory . "/home/ACCELERATION/nathan/projects/spat/pynpact/pynpact"))))
--- spatweb/spat/views/run.py
+++ spatweb/spat/views/run.py
@@ -11,11 +11,12 @@ from django.contrib import messages
 
 from __init__ import session_key
 
-from pynpact import main
+from pynpact import prepare
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
 def view(request):
+    path = request.session[session_key("input_path")]
     return render_to_response('run.html',{},
                                context_instance=RequestContext(request))
