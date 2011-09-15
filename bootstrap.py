9ff8fbb2a24aaec38f594a88a60c06556cbf6383
Nathan Bird <nathan@acceleration.net>
2011-09-15 17:43:53 -0400

    Reorganizing to get rid of 'requirements directory and instead put that stuff in 'lib'

--- bootstrap.py
+++ bootstrap.py
@@ -11,7 +11,7 @@ vedir = os.path.abspath(os.path.join(pwd,"ve"))
 if os.path.exists(vedir):
     shutil.rmtree(vedir)
 
-virtualenv_support_dir = os.path.abspath(os.path.join(pwd, "requirements", "virtualenv_support"))
+virtualenv_support_dir = os.path.abspath(os.path.join(pwd, "lib", "virtualenv_support"))
 
 ret = subprocess.call(["python", "virtualenv.py", 
                        "--extra-search-dir=%s" % virtualenv_support_dir,
@@ -24,20 +24,21 @@ if ret: exit(ret)
 ret = subprocess.call([os.path.join(vedir, 'bin', 'pip'), "install",
                        "-E", vedir,
                        "--index-url=''",
-                       "--requirement",os.path.join(pwd,"requirements/libs.txt")])
+                       "--requirement",os.path.join(pwd,"requirements.txt")])
 if ret: exit(ret)
 
 
-the_eggs = [os.path.basename(path) for path in
-            glob.glob(os.path.join(pwd, "requirements", "eggs", "*.egg"))]
-if the_eggs:
-    # only try to easy install eggs if there actually are some
-    cmd = ([os.path.join(vedir,"bin/easy_install"),
-            '-f', os.path.join(pwd,"requirements/eggs/")] +
-           the_eggs)
-    ret = subprocess.call(cmd)
+## no eggs 
+# the_eggs = [os.path.basename(path) for path in
+#             glob.glob(os.path.join(pwd, "requirements", "eggs", "*.egg"))]
+# if the_eggs:
+#     # only try to easy install eggs if there actually are some
+#     cmd = ([os.path.join(vedir,"bin/easy_install"),
+#             '-f', os.path.join(pwd,"requirements/eggs/")] +
+#            the_eggs)
+#     ret = subprocess.call(cmd)
 
-if ret: exit(ret)
+# if ret: exit(ret)
 
 pynpact_dir = os.path.join(os.path.dirname(__file__), "pynpact/")
 subprocess.call(["make"],cwd=pynpact_dir)
