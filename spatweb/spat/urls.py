81a1807c8a971463e4dad1235becabdebacd85be
Nathan Bird <nathan@acceleration.net>
2011-10-06 16:51:57 -0400

    Got very basic templates for 404 and 500 errors. Need to still have an error report form. re #572 (0.75)

--- spatweb/spat/urls.py
+++ spatweb/spat/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 urlpatterns = \
             patterns('spat.views',
-                     url(r'^$', 'index'),
+                     url(r'^$', 'index', name='index'),
                      url(r'^start$', 'start.view', name="start"),
                      url(r'^start/efetch/(\d+)', 'start.efetch', name="efetch"),
                      url(r'^library', 'library'),
@@ -18,10 +18,3 @@ if settings.DEBUG :
     urlpatterns += patterns('', url(r'^raw/(?P<path>.*)$', 'django.views.static.serve', {
         'document_root': settings.MEDIA_ROOT,
         }, name="raw"))
-
-else :
-    #TODO: "Need to configure a fallback url for apache to punt to."
-    raise Exception("Need to configure a fallback url for apache to punt to.")
-
-
-
