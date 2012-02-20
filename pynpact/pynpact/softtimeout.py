7b393d9becc1de822d1140be7f742993b5446d7a
Nathan Bird <nathan@acceleration.net>
2012-02-20 10:30:51 -0500

    Adding softtimeout module that's required for finegrained checking already committed.

---
+++ pynpact/pynpact/softtimeout.py
@@ -0,0 +1,42 @@
+import time
+
+class Timeout(Exception):
+    def __init__(self, **args):
+        self.__dict__.update(args)
+
+class SoftTimer(object):
+    deadline = None
+    starttime = None
+    checkcount = 0
+    steps = None
+
+    def __init__(self, timeout=None, **kwargs):
+        self.set_timeout(timeout,from_now=True)
+        self.steps=[]
+        super(SoftTimer, self).__init__(**kwargs)
+
+
+    def set_timeout(self, timeout, from_now=False):
+        if from_now:
+            self.starttime = time.time()
+        if timeout and timeout > 0:
+            self.deadline = self.starttime + timeout
+
+    def check(self, step=None, logfn=None, **kwargs):
+        
+        self.checkcount +=1
+        if step:
+            self.steps.append(step)
+
+        if logfn:
+            logfn("Checking for timeout, %d time: %r", self.checkcount, step)
+
+        t2 = time.time()
+        if self.deadline and t2 > self.deadline:
+            raise Timeout(tdiff=(t2 - self.starttime),
+                          checkcount=self.checkcount,
+                          steps = self.steps,
+                          **kwargs)
+
+
+
