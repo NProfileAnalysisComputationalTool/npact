d21aeec82ec394057cc749671bbc1b9f7c3eeb57
Nathan Bird <nathan@acceleration.net>
2012-03-01 17:52:12 -0500

    Converting CG.c into nprofile.c
    it now takes option [-b bases] which is the list of bases to calculate
    the profile for. It defaults to "cg".

--- pynpact/src/nprofile.c
+++ pynpact/src/nprofile.c
@@ -11,7 +11,7 @@
 
 #define ORDER 0
 
-const char usage[]= "\nUsage: CG input.gbk [start end [window_size step [period_of_frames]]]\nDefaults: start= 1; end= end of genome sequence; window_size= 201; step= 51; period_of_frames= 3.\n";
+const char usage[]= "\nUsage: [-b bases] input.gbk [start end [window_size step [period_of_frames]]]\nDefaults: start= 1; end= end of genome sequence; window_size= 201; step= 51; period_of_frames= 3.\n";
 
 
 int mapfile(char* filename, char** addr, size_t* length) {
@@ -49,8 +49,8 @@ int mapfile(char* filename, char** addr, size_t* length) {
 }
 
 void calculateProfile(const char* origin, const size_t length,
-                      const int start, const int end, const int window,
-                      const int step, const int period) {
+                      const char* bases, const int start, const int end,
+                      const int window, const int step, const int period) {
 
    char *box, base;
    int offset =0 , j, baseidx=0, n=0;
@@ -58,8 +58,8 @@ void calculateProfile(const char* origin, const size_t length,
    double normfactor = 100.0/(double)(window/period);
 
    fprintf(stderr,
-           "Reading bases %d-%d with window %d nt, step %d nt and period %d nt.\n",
-           start, end, window, step, period);
+           "Calculating [%s]-profile of bases %d-%d with window %d nt, step %d nt and period %d nt.\n",
+           bases, start, end, window, step, period);
 
    box = (char *)calloc(window, sizeof(char));
    S = (size_t *)calloc(period, sizeof(size_t));
@@ -80,7 +80,7 @@ void calculateProfile(const char* origin, const size_t length,
       base = origin[offset];
       if(base >= 'a' && base <= 'z') {
          ++baseidx;
-         if(base=='c' || base=='g') {
+         if(strchr(bases, base)) {
             if(! box[n % window]) {
                //if the flag wasn't set, then set it and increase the count.
                ++S[(baseidx-1) % period];
@@ -95,7 +95,7 @@ void calculateProfile(const char* origin, const size_t length,
             }
 
             //unexpected base, print warning.
-            if(! (base=='a' || base=='t' || base=='u'))
+            if(! strchr("actgu", base))
                fprintf(stderr,"\nBase %c found at position %d\n",base,baseidx);
          }
          ++n;
@@ -124,16 +124,25 @@ int countBases(const char* origin, const size_t length) {
 
 int main(int argc, char *argv[]) {
    int start=1,end=0,window=201,step=51,period=3,tot=0;
+   int i;
    char* mapped_file;
    char* origin;
    size_t length, olength;
    int argi = 1;
    char* filename;
+   char* bases = "cg";
 
 
    if(!(argc==2 || argc==4 || argc==6 || argc==7)) {
       fputs(usage, stderr);
       exit(1);
+   }
+   if(strcmp(argv[argi], "-b") == 0) {
+      bases = (char*) argv[++argi];
+      for (i = 0; bases[i]; i++)
+         if (bases[i] < 'a')
+            bases[i] = bases[i] + 'a' - 'A';
+      argi++;
    }
    filename = argv[argi++];
 
@@ -179,7 +188,7 @@ int main(int argc, char *argv[]) {
       exit(1);
    }
 
-   calculateProfile(origin, olength, start, end, window, step, period);
+   calculateProfile(origin, olength, bases, start, end, window, step, period);
 
    munmap(mapped_file, length);
    exit(0);
