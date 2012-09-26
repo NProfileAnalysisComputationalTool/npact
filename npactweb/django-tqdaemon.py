5ef37a99c907dc1215a35dd10e8733b675e96d13
Nathan Bird <nathan@acceleration.net>
2012-09-26 10:23:54 -0400

    removing incorrect comments at the top of django-tqdaemon.

--- npactweb/django-tqdaemon.py
+++ npactweb/django-tqdaemon.py
@@ -1,19 +1,5 @@
 #!/usr/bin/env python
 
-# We have two copies of this file to correspond with two process pools
-# managed by apache. Apache requires a different target in order to
-# start a second process pool (class).
-#
-# There are two process pools:
-#
-# * One handles general website traffic that is expected to complete
-#   quickly
-# * One handles all calls to the underlying C which could potentially
-#   be slow to respond.
-
-
-#this is supposed to work, if it doesn't remember this address:
-# http://stackoverflow.com/questions/527237/unhandled-exception-in-flup
 import os
 import sys
 import logging
