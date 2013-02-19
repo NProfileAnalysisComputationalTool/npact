198c470623d39b4d0f5e5b83a7303c6e8e3f18b7
Luciano Brocchieri <lucianob@ufl.edu>
2013-02-19 17:01:48 -0500

    Changed password in settings.py

--- npactweb/settings.py
+++ npactweb/settings.py
@@ -31,7 +31,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'npact1.0@gmail.com'
-EMAIL_HOST_PASSWORD = 'sictransit'
+EMAIL_HOST_PASSWORD = 'sictransit2'
 
 
 
