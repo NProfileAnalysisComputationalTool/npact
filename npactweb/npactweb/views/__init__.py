1dc45155da56e3682910a9a9082ea4d8b02316cf
Nathan Bird <nathan@acceleration.net>
2014-05-28 14:04:36 -0400

    whitespace-cleanup

--- npactweb/npactweb/views/__init__.py
+++ npactweb/npactweb/views/__init__.py
@@ -14,34 +14,34 @@ from npactweb import library_root
 logger = logging.getLogger(__name__)
 
 
-##### Views that are small enough to be inline here.
+#### Views that are small enough to be inline here.
 
 def static_serve_wrapper(request, path):
     if settings.DEBUG:
         return static.serve(request, path=path, document_root=settings.MEDIA_ROOT)
-        
 
-def index(request) :
-    return render_to_response('index.html',{},
+
+def index(request):
+    return render_to_response('index.html', {},
                                context_instance=RequestContext(request))
 
-def library(request) :
+def library(request):
     files = []
-    for gbk in glob.iglob(os.path.join(library_root(),"*.gbk")) :
+    for gbk in glob.iglob(os.path.join(library_root(), "*.gbk")):
         data = prepare.try_parse(gbk)
 
-        if not data.get('url') :
-            data['url'] =  reverse('run', args=['library/' + data['basename']])
+        if not data.get('url'):
+            data['url'] = reverse('run', args=['library/' + data['basename']])
 
         files.append(data)
 
     logger.debug("Found %d files in library", len(files))
     return render_to_response('library.html',
-                              {'files':files},
+                              {'files': files},
                               context_instance=RequestContext(request))
 
 
-def view_none(request) :
+def view_none(request):
     messages.error(request,
                    "No genome source selected, please upload one, "
                    "or go to the library and select one.")
