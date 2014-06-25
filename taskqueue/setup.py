501002f7029498533ab8c2df282c9d2f0553fdb2
Nathan Bird <nathan@acceleration.net>
2014-06-25 09:40:05 -0400

    TQ: use setuptools and pytest
    This brings in py.test and mock as test dependencies and tries to use
    the [best practices][1] for integration
    
    [1]: http://pytest.org/latest/goodpractises.html#integrating-with-distutils-python-setup-py-test

--- taskqueue/setup.py
+++ taskqueue/setup.py
@@ -1,21 +1,39 @@
 #!/usr/bin/env python
-from distutils.core import setup
-import os, os.path
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+import sys
 
 
-def recursive_files(*roots):
-    for r in roots:
-        for root,directories,files in os.walk(r):
-            for i in files:
-                yield os.path.join(root,i)
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = None
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+description = """
+Basic process pool daemon that can run python tasks asynchronously
+""".strip()
 
 setup(name='taskqueue',
-      version='0.2',
-      description='Basic process pool daemon that can run python tasks asynchronously',
+      version='0.2.1',
+      description=description,
       author='Nathan Bird',
       author_email='nathan@acceleration.net',
       url='http://genome.ufl.edu/npact/',
       packages=['taskqueue'],
-      requires=["lockfile",],
-      scripts=['tqdaemon.py']
-     )
+      requires=["lockfile"],
+      tests_require=["pytest>=2.4", "mock>=1"],
+      scripts=['tqdaemon.py'],
+      cmdclass={'test': PyTest})
