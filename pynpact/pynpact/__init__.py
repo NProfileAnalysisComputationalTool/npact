58f50a6de79b8114cc2f062cfe3451a222c619da
Nathan Bird <nathan@acceleration.net>
2014-07-01 14:56:08 -0400

    pep8 improvements

--- pynpact/pynpact/__init__.py
+++ pynpact/pynpact/__init__.py
@@ -3,19 +3,26 @@ import os.path
 
 from pkg_resources import resource_filename
 
+
 class NullHandler(logging.Handler):
+    "Swallow all logging messages"
     def emit(self, record):
         pass
 
+# add a NullHandler so that we don't get "logging not configured"
+# messages
 logging.getLogger('pynpact').addHandler(NullHandler())
 
 
-BINPATH=os.path.realpath(os.path.join(os.path.dirname(__file__),"bin"))
+BINPATH = os.path.realpath(
+    os.path.join(os.path.dirname(__file__), "bin"))
 
-def binfile(name) :
+
+def binfile(name):
     path = os.path.join(BINPATH, name)
-    assert os.path.exists(path), "Missing pynpact/bin/" + name + ", perhaps you need to run make?"
-    return  path
+    assert os.path.exists(path), \
+        "Missing pynpact/bin/" + name + ", perhaps you need to run make?"
+    return path
 
 
-DATAPATH=resource_filename(__name__, 'data')
+DATAPATH = resource_filename(__name__, 'data')
