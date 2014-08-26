f2448354a452a0461b3f8ce33799c6897ea9bbc8
Ryan Davis <ryan@acceleration.net>
2014-08-26 12:37:24 -0400

    GraphCalculator: refactor a bit
    more named functions, remove dead code
    
    refs Programming:#742 (.125)

--- npactweb/npactweb/static/js/graphs/calculator.js
+++ npactweb/npactweb/static/js/graphs/calculator.js
@@ -1,6 +1,77 @@
 (function(){
 
-  function GraphingCalculator(K, $log, Utils){
+  /**
+   * calculate measurements about the chart
+   */
+  function chartMetrics(opts){
+    var yStops = [100, 80, 60, 40, 20, 0],
+	yAxisTicks = yStops.length - 1,
+	// the line graph, excluding tick marks and axis labels
+	g = {
+	  x: opts.leftPadding,
+	  y: opts.stageHeight - opts.profileHeight
+	    - opts.axisLabelFontsize // x-axis labels
+	    - 2*opts.profileTicks, // tick marks
+	  h: opts.profileHeight,
+	  w: opts.stageWidth - opts.leftPadding
+	    - opts.rightPadding
+	},
+	yAxisTickSpacing = g.h / yAxisTicks,
+	yAxisTickX = g.x - opts.profileTicks,
+	yticks = yStops.map(makeYTick),
+	// space between the label and the tick
+	yAxisLabelRightPadding = opts.profileTicks*2,
+	yAxisLabelWidth = opts.leftPadding - yAxisLabelRightPadding,
+	ylabels = yStops.map(makeYLabel),
+	// box to center the title inside of
+	ytitle={
+	  x: 0, y: g.y, width: yAxisLabelWidth, height: g.h
+	}
+    ;
+    
+    return {
+      graph:g,
+      // TODO: return xaxis here
+      yaxis:{
+	ticks: yticks,
+	labels: ylabels,
+	titleBox: ytitle
+      }
+    };
+
+    function makeYTick(lbl, n){
+      var y = g.y + n*yAxisTickSpacing;
+      return {
+	x: yAxisTickX, y: parseInt(y),
+	x2: g.x, y2: parseInt(y)
+      };
+    }
+    
+    function makeYLabel(lbl, n){
+      // center on the tick
+      var y = yticks[n].y - (opts.axisLabelFontsize/2);
+      return {
+	x: 0, y: parseInt(y),
+	width: yAxisLabelWidth,
+	text: lbl
+      }; 
+    }
+  }
+
+  /**
+   * find a position that will align the two rectangles on their center
+   *
+   * @param {Object} rect - the rectangle to align to {width,height}
+   * @param {Object} toAlign - the rectangle you want to move {width,height}
+   * @returns {Object} x,y coordinates to shift `toAlign` by, from the top/left
+   */
+  function alignRectangles(rect, toAlign){
+    // get the top left coordinate of the `toAlign` rect
+    return {x: rect.x + (rect.width/2) - (toAlign.width/2),
+	    y: rect.y + (rect.height/2) - (toAlign.height/2)};
+  }
+  
+  function GraphingCalculator(K, Utils){
    
     function stops(a,b){
       var length = b - a,
@@ -16,95 +87,22 @@
 	stops: _.range(start, end+interval, interval)
       };
     }
-    
-    function chart(opts){
-      var yStops = [100, 80, 60, 40, 20, 0],
-	  yAxisTicks = yStops.length - 1,
-	  // the line graph, excluding tick marks and axis labels
-	  g = {
-	    x: opts.leftPadding,
-	    y: opts.stageHeight - opts.profileHeight
-	      - opts.axisLabelFontsize // x-axis labels
-	      - 2*opts.profileTicks, // tick marks
-	    h: opts.profileHeight,
-	    w: opts.stageWidth - opts.leftPadding
-	      - opts.rightPadding
-	  },
-	  yAxisTickSpacing = g.h / yAxisTicks,
-	  yAxisTickX = g.x - opts.profileTicks,
-	  yticks = yStops.map(function(lbl, n){
-	    var y = g.y + n*yAxisTickSpacing;
-	    return {
-	      x: yAxisTickX, y: parseInt(y),
-	      x2: g.x, y2: parseInt(y)
-	    };
-	  }),
-	  // space between the label and the tick
-	  yAxisLabelRightPadding = opts.profileTicks*2,
-	  yAxisLabelWidth = opts.leftPadding - yAxisLabelRightPadding,
-	  ylabels = yStops.map(function(lbl, n){
-	    // center on the tick
-	    var y = yticks[n].y - (opts.axisLabelFontsize/2);
-	    return {
-	      x: 0, y: parseInt(y),
-	      width: yAxisLabelWidth,
-	      text: lbl
-	    };
-	  }),
-	  // box to center the title inside of
-	  ytitle={
-	    x: 0, y: g.y, width: yAxisLabelWidth, height: g.h
-	  }
-      ;
-
-      return {
-	graph:g,
-	// TODO: return xaxis here
-	yaxis:{
-	  ticks: yticks,
-	  labels: ylabels,
-	  titleBox: ytitle
-	}
-      };
-    }
-
-
-    // return a top left x/y position that aligns
-    // the centers of the two rectangles
-    function alignRectangles(rect, toAlign){
-      // get the top left coordinate of the `toAlign` rect
-      return {x: rect.x + (rect.width/2) - (toAlign.width/2),
-	      y: rect.y + (rect.height/2) - (toAlign.height/2)};
-    }
 
     function xaxis(opts){
-      var m = chart(opts),
+      var m = chartMetrics(opts),
 	  s = stops(opts.range[0], opts.range[1]),
 	  length = opts.range[1] - opts.range[0],
-	  interval = s.interval, ss = s.stops,
+	  interval = s.interval,
+	  ss = s.stops,
 	  // want to capture some margin
 	  start = ss[0],
 	  end = _.last(ss),
 	  xAxisTicks = ss.length,
-	  xAxisTickY = m.graph.y + m.graph.h,
-	  ticks = ss.map(function(lbl, n){
-	    var x = parseInt(start + n*interval);
-	    return {
-	      x: x, y: 0,
-	      x2: x, y2: opts.profileTicks
-	    };
-	  }),
+	  ticks = ss.map(makeTicks),
 	  scaleX = m.graph.w / length,
 	  // blow the text back up, we don't want it scaled
 	  labelScaleX = 1/scaleX,
-	  labels = ss.map(function(lbl, n){
-	    return {
-	      x: ticks[n].x, y: opts.profileTicks,
-	      coord: ticks[n].x,
-	      text: lbl,
-	      scaleX: labelScaleX
-	    };
-	  })
+	  labels = ss.map(makeLabels)
       ;
 
       return {
@@ -119,6 +117,23 @@
 	ticks: ticks,
 	labels:labels
       };
+
+      function makeTicks(lbl, n){
+	var x = parseInt(start + n*interval);
+	return {
+	  x: x, y: 0,
+	  x2: x, y2: opts.profileTicks
+	};
+      }
+
+      function makeLabels(lbl, n){
+	return {
+	  x: ticks[n].x, y: opts.profileTicks,
+	  coord: ticks[n].x,
+	  text: lbl,
+	  scaleX: labelScaleX
+	};
+      }
     }
 
     function zoom(obj){
@@ -208,7 +223,7 @@
     };
 
     return {
-      chart:chart,
+      chart:chartMetrics,
       xaxis:xaxis,
       alignRectangles:alignRectangles,
       stops:stops,
