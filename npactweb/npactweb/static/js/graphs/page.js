7393663ed64b1fa455230933f5942f5300327002
Nathan Bird <nathan@acceleration.net>
2014-08-28 14:06:52 -0400

    whitespace-cleanup

--- npactweb/npactweb/static/js/graphs/page.js
+++ npactweb/npactweb/static/js/graphs/page.js
@@ -9,13 +9,12 @@ angular.module('npact')
 	// TODO: watch for changes in width
 	// http://stackoverflow.com/questions/23044338/window-resize-directive
 	Utils.widthAvailable($element).then(GraphDealer.setWidth);
-
-	$http.get(STATIC_BASE_URL+'js/nprofile.json').then(function(res){
-	  GraphDealer.setProfile(res.data);
-	});
-	$http.get(STATIC_BASE_URL+'js/extract.json').then(function(res){
-	  GraphDealer.addExtract('Input file CDS', res.data);
-	});
+        $http.get(STATIC_BASE_URL + 'js/nprofile.json').then(function(res) {
+          GraphDealer.setProfile(res.data);
+        });
+        $http.get(STATIC_BASE_URL + 'js/extract.json').then(function(res) {
+          GraphDealer.addExtract('Input file CDS', res.data);
+        });
       }
     };
   });
