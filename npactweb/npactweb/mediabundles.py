bfb25bff43cdb188f389c99839a6f6fda479afae
Ryan Davis <ryan@acceleration.net>
2014-09-12 15:34:32 -0400

    remove dead processing.js
    refs Programming:#742

--- npactweb/npactweb/mediabundles.py
+++ npactweb/npactweb/mediabundles.py
@@ -22,7 +22,6 @@ MEDIA_BUNDLES = (
 
     ('processing.js',
      'bower_components/kineticjs/kinetic.min.js',
-     'js/processing.js',
      'bower_components/angular/angular.js',
      'bower_components/lodash/dist/lodash.min.js',
      'bower_components/qtip2/jquery.qtip.min.js',
