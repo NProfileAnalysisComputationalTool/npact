223d141ee1cf4df7f5f21aa4c7b22e5f1c0ee4cd
Ryan Davis <ryan@acceleration.net>
2014-10-21 12:51:12 -0400

    `grunt jshint` lints the JS source

--- npactweb/Gruntfile.js
+++ npactweb/Gruntfile.js
@@ -1,9 +1,24 @@
-'use strict';
-
 module.exports = function(grunt){
+  'use strict';
   require('load-grunt-tasks')(grunt);
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  var jsFiles = ['npactweb/static/js/**/*.js'];
 
   grunt.initConfig({
+    jshint: {
+      options: {
+        jshintrc: true
+      },
+      all: jsFiles,
+      jenkins: {
+        files: {src: jsFiles},
+        options: {
+          reporter: 'checkstyle',
+          reporterOutput: 'jshint.xml',
+          force: true
+        }
+      }
+    },
     karma: {
       unit: {
 	configFile: 'karma.conf.js'
