5a2dd066c3506ecb8efd62b270051bb14c12daa3
Ryan Davis <ryan@acceleration.net>
2014-11-11 12:28:24 -0500

    simplify `npact-graph-container`
    * cache the headerSpec, share amongst child `npact-graph`s
    * use named helper functions to make the watch/event hooks easier to
      read
    * `npact-graph` can only exist inside a `npact-graph-container`
    
    neglible performance difference
    
    refs #846 (.5)

--- npactweb/npactweb/static/js/graphs/graph.js
+++ npactweb/npactweb/static/js/graphs/graph.js
@@ -1,11 +1,78 @@
 angular.module('npact')
 
-  .controller('npactGraphContainerCtrl', function($scope, npactConstants, Utils, GraphConfig, ProfileReader, Evt, $log) {
+  .controller('npactGraphContainerCtrl', function($scope, npactConstants, Utils, GraphConfig, ProfileReader, Evt, $log, GraphingCalculator) {
     var self = this,
         graphUpdateStart = null, graphsDrawn = 0,
         visibleGraphs = 5,
         graphSpecs = [],
-        getGraphConfig = function() { return GraphConfig; }
+        getGraphConfig = function() { return GraphConfig; },
+        getProfileSummary = function() {
+          try {return ProfileReader.summary();} catch(e) { }
+          return null;
+        },
+        makeGraphOptions = function() {
+          var opts = angular.extend(
+            {},
+            npactConstants.graphSpecDefaults,
+            {
+              headerSpec: GraphConfig.headerSpec(),
+              margin: Utils.orderOfMagnitude(GraphConfig.basesPerGraph, -1),
+              width: GraphConfig.width,
+              colors: GraphConfig.colorBlindFriendly ?
+                npactConstants.colorBlindLineColors : npactConstants.lineColors
+            });
+          opts.headerY = opts.headerSpec.headerY;
+          return opts;
+        },
+        onPan = function(evt, opts) {
+          var offset = Math.floor(opts.newStartBase - opts.oldStartBase);
+          GraphConfig.offset += offset;
+          // TODO: Event originated from outside ng, but why doesn't
+          // `$watch` pick up the `offset` change?
+          $scope.$apply();
+        },
+        onZoom = function(evt, opts) {
+          var zoomOpts = angular.extend({}, opts, GraphConfig),
+              res = GraphingCalculator.zoom(zoomOpts);
+          GraphConfig.offset = res.offset;
+          GraphConfig.basesPerGraph = res.basesPerGraph;
+          // TODO: Event originated from outside ng, but why doesn't
+          // `$watch` pick up the `offset` change?
+          $scope.$apply();
+        },
+        onGraphRedrawComplete = function(evt) {
+          graphsDrawn++;
+          if(graphsDrawn === $scope.graphSpecs.length){
+            $log.log('graphs done', new Date() - graphUpdateStart, 'ms');
+          }
+        },
+        onGraphConfigChanged = function(newValue, oldValue){
+          var cmd = newValue.refreshCommand(oldValue);
+          $log.log('graph config changed:', cmd);
+          graphUpdateStart = new Date();
+          graphsDrawn = 0;
+          self.graphOptions = makeGraphOptions();
+          switch(cmd){
+          case Evt.REBUILD:
+            graphSpecs = ProfileReader.partition(GraphConfig);
+            $scope.graphSpecs = _.take(graphSpecs, visibleGraphs);
+            break;
+          case Evt.REDRAW:
+            $scope.$broadcast(cmd);
+            break;
+          }
+        },
+        onProfileSummaryChanged = function(summary, oldSummary) {
+          if(summary){
+            // find a sensible zoom level
+            var basesPerGraph = summary.length / 5;
+            // if we're really short, reset out bases per graph
+            if (basesPerGraph < GraphConfig.basesPerGraph) {
+              GraphConfig.basesPerGraph = Utils.orderOfMagnitude(basesPerGraph);
+            }
+            GraphConfig.profileSummary = summary;
+          }
+        }
     ;
 
     $scope.graphHeight = npactConstants.graphSpecDefaults.height;
@@ -20,97 +87,35 @@ angular.module('npact')
       }
     };
 
-    $scope.$on(Evt.PAN, function(evt, opts) {
-      var offset = Math.floor(opts.newStartBase - opts.oldStartBase);
-      GraphConfig.offset += offset;
-      // TODO: Event originated from outside ng, but why doesn't
-      // `$watch` pick up the `offset` change?
-      $scope.$apply();
-    });
+    // listen for graph events
+    $scope.$on(Evt.PAN, onPan);
+    $scope.$on(Evt.ZOOM, onZoom);
+    $scope.$on(Evt.GRAPH_REDRAW_COMPLETE, onGraphRedrawComplete);
 
-    $scope.$on(Evt.ZOOM, function(evt, opts) {
-      var res = GraphingCalculator.zoom(angular.extend({}, opts, GraphConfig));
-      GraphConfig.offset = res.offset;
-      GraphConfig.basesPerGraph = res.basesPerGraph;
-      // TODO: Event originated from outside ng, but why doesn't
-      // `$watch` pick up the `offset` change?
-      $scope.$apply();
-    });
-
-    $scope.$on(Evt.GRAPH_REDRAW_COMPLETE, function(evt) {
-      graphsDrawn++;
-      if(graphsDrawn === $scope.graphSpecs.length){
-        $log.log('graphs done', new Date() - graphUpdateStart, 'ms');
-      }
-    });
-
-    $scope.$watch(getGraphConfig, function(newValue, oldValue){
-      var cmd = newValue.refreshCommand(oldValue);
-      $log.log('graph config changed:', cmd);
-      graphUpdateStart = new Date();
-      graphsDrawn = 0;
-      switch(cmd){
-      case Evt.REBUILD:
-        graphSpecs = ProfileReader.partition(GraphConfig);
-        $scope.graphSpecs = _.take(graphSpecs, visibleGraphs);
-        break;
-      case Evt.REDRAW:
-        $scope.$broadcast(cmd);
-        break;
-      }
-    }, true); // deep-equality
-
-
-    $scope.$watchCollection(
-      function() {
-        try {return ProfileReader.summary();} catch(e) { }
-        return null;
-      },
-      function(summary, oldSummary) {
-        if(summary){
-          // find a sensible zoom level
-          var basesPerGraph = summary.length / 5;
-          // if we're really short, reset out bases per graph
-          if (basesPerGraph < GraphConfig.basesPerGraph) {
-            GraphConfig.basesPerGraph = Utils.orderOfMagnitude(basesPerGraph);
-          }
-          GraphConfig.profileSummary = summary;
-        }
-      });
+    // watch the environment for changes we care about
+    $scope.$watch(getGraphConfig, onGraphConfigChanged, true); // deep-equality
+    $scope.$watchCollection(getProfileSummary, onProfileSummaryChanged);
   })
   .directive('npactGraphContainer', function(STATIC_BASE_URL) {
     return {
       restrict: 'A',
       scope: {},
       templateUrl: STATIC_BASE_URL + 'js/graphs/graph-container.html',
-      controller:'npactGraphContainerCtrl',
-      controllerAs: 'ctrl'
+      controller: 'npactGraphContainerCtrl as ctrl'
     };
   })
 
-  .directive('npactGraph', function npactGraph($log, Grapher, Evt, GraphConfig, Utils, npactConstants, GraphingCalculator){
+  .directive('npactGraph', function npactGraph(Grapher, Evt, GraphingCalculator){
     return {
       restrict: 'A',
-      scope:{
-        spec: '=npactGraph'
-      },
-      controller: function($scope, $element, $attrs){
+      scope: { spec: '=npactGraph' },
+      require: '^npactGraphContainer',
+      link: function($scope, $element, $attrs, ctrl){
         var g = null, buildOptions = function(range) {
           var opts = angular.extend(
-            {},
-            npactConstants.graphSpecDefaults,
-            range,
-            {
-              $scope: $scope,
-              element: $element[0],
-              headerSpec: GraphConfig.headerSpec(),
-              margin: Utils.orderOfMagnitude(GraphConfig.basesPerGraph, -1),
-              width: GraphConfig.width,
-              colors: GraphConfig.colorBlindFriendly ?
-                npactConstants.colorBlindLineColors : npactConstants.lineColors
-            });
+            {}, ctrl.graphOptions, range,
+            {$scope: $scope, element: $element[0]});
 
-          opts.headerY = opts.headerSpec.headerY;
           opts.m = GraphingCalculator.chart(opts);
           opts.xaxis = GraphingCalculator.xaxis(opts);
           return opts;
