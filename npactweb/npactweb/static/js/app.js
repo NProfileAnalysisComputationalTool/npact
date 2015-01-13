18c3409b31aec2dfa62cf69cd92b037b2149b0a3
Nathan Bird <nathan@acceleration.net>
2015-01-13 17:15:15 -0500

    Put the $locationProvider into html5 mode
    I need this so I can pull querystring variables
    
    E.g. /npact/run/library/NC_007760.gbk?endBase=19994 will go through
    endBase of 1994 otherwise that needs to be after the hash.
    
    re #863 (0.25)

--- npactweb/npactweb/static/js/app.js
+++ npactweb/npactweb/static/js/app.js
@@ -1,5 +1,8 @@
 angular.module('npact', ['ngMessages', 'sticky'])
   .value('K', Kinetic)
+  .config(function($locationProvider) {
+    $locationProvider.html5Mode({requireBase: false, enabled: true});
+  })
   .run(function($rootScope, STATIC_BASE_URL) {
     $rootScope.STATIC_BASE_URL = STATIC_BASE_URL;
   })
