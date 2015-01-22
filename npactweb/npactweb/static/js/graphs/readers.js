5c323bc361bcba1a3b21f7f08b30d9c8870c750b
Nathan Bird <nathan@acceleration.net>
2015-01-22 11:02:26 -0500

    Readers: Use more promises to solve race condition
    Previously loaded tracks only got added to the dictionary when they were
    done parsing, but if someone tried to use it before then it got the
    TrackNotFound error.
    
    Instead we now stick the parsing promise into the dictionary and use it
    to get the data when it comes time to slice.
    
    re #847 (0.75)

--- npactweb/npactweb/static/js/graphs/readers.js
+++ npactweb/npactweb/static/js/graphs/readers.js
@@ -1,115 +1,115 @@
 angular.module('npact')
 
   .service('TrackReaderRTreeIndex', function(ExtractParser, $log, Utils, Err, $q, $timeout){
-    var self = this;
-
-    self.tracks = {};
+    this.tracks = {};
     /**
      * load the given track name and data
      */
     this.load = function(name, data) {
-      return ExtractParser.parseAsync(data)
-        .then(
-          function(data) {
-            var index = RTree();
-            return Utils.forEachAsync(data, function(d) {
-              index.insert({ x: d.start, y: 0,
-                             w: d.end - d.start, h: 5},
-                           d);
-            }).then(function() { self.tracks[name] = index; });
-          },
-          function(e) { $log.log('TrackReader.load failed', name, e); throw e; }
-        );
+      return (
+        this.tracks[name] =
+          ExtractParser.parseAsync(data)
+          .then(
+            function(data) {
+              var index = RTree();
+              return Utils.forEachAsync(data, function(d) {
+                index.insert({ x: d.start, y: 0,
+                               w: d.end - d.start, h: 5},
+                             d);
+              }).then(function() { return index; });
+            },
+            function(e) { $log.log('TrackReader.load failed', name, e); throw e; }
+          ));
     };
 
     this.slice = function(opts) {
-      if (!_.has(self.tracks, opts.name)) { throw new Err.TrackNotFound(); }
-      return $q(function(resolve, reject) {
-        $timeout(function() {
-          resolve(self.tracks[opts.name].bbox(opts.startBase, 0, opts.endBase, 5));
+      var track = this.tracks[opts.name];
+      if (!track) { throw new Err.TrackNotFound(); }
+      return track.then(function(index) {
+        return $timeout(function() {
+          return index.bbox(opts.startBase, 0, opts.endBase, 5);
         });
       });
     };
   })
   .service('TrackReaderNaive', function(ExtractParser, $log, Utils, Err, $q, $timeout){
-    var self = this;
-
-    self.tracks = {};
+    this.tracks = {};
     /**
      * load the given track name and data
      */
     this.load = function(name, data) {
-      return ExtractParser.parseAsync(data)
-        .then(
-          function(data) { return (self.tracks[name] = data); },
-          function(e) { $log.log('TrackReader.load failed', name, e); throw e; }
-        );
+      return (
+        this.tracks[name] = ExtractParser.parseAsync(data)
+          .catch(
+            function(e) { $log.log('TrackReader.load failed', name, e); throw e; }
+          ));
     };
 
     this.slice = function(opts) {
-      if (!_.has(self.tracks, opts.name)) { throw new Err.TrackNotFound(); }
+      if (!_.has(this.tracks, opts.name)) { throw new Err.TrackNotFound(); }
       $log.log('TrackReader.slice', opts);
 
-      var minIdx = null, maxIdx, data = self.tracks[opts.name];
-      // TODO: use _.sortedIndex to binary search, or some other
-      // better index structure/algorithm
-      return Utils.forEachAsync(data, function(dataPoint, idx) {
-        // extract starts in this range?
-        var startsInRange = dataPoint.start >= opts.startBase &&
-              dataPoint.start <= opts.endBase,
-            // extract ends in this range?
-            endsInRange = dataPoint.end >= opts.startBase &&
-              dataPoint.end <= opts.endBase;
-        if(startsInRange || endsInRange){
-          if(minIdx === null) { minIdx = idx; }
-          maxIdx = idx;
-        }
-      }).then(function() {
-        return minIdx === null ? [] : data.slice(minIdx, maxIdx+1);
+      var minIdx = null, maxIdx, data = this.tracks[opts.name];
+      return this.tracks[opts.name].then(function(data) {
+        // TODO: use _.sortedIndex to binary search, or some other
+        // better index structure/algorithm
+        return Utils.forEachAsync(data, function(dataPoint, idx) {
+          // extract starts in this range?
+          var startsInRange = dataPoint.start >= opts.startBase &&
+                dataPoint.start <= opts.endBase,
+              // extract ends in this range?
+              endsInRange = dataPoint.end >= opts.startBase &&
+                dataPoint.end <= opts.endBase;
+          if(startsInRange || endsInRange){
+            if(minIdx === null) { minIdx = idx; }
+            maxIdx = idx;
+          }
+        }).then(function() {
+          return minIdx === null ? [] : data.slice(minIdx, maxIdx+1);
+        });
       });
     };
   })
   .service('TrackBinarySearch', function(ExtractParser, $log, Utils, Err, $q, $timeout) {
-    var self = this;
-    self.tracks = {};
+    this.tracks = {};
 
-        /**
+    /**
      * load the given track name and data
      */
     this.load = function(name, data) {
-      return ExtractParser.parseAsync(data)
+      return (this.tracks[name] = ExtractParser.parseAsync(data)
         .then(
           function(data) {
             var byStart = _.sortBy(data, 'start');
-            return (self.tracks[name] = {
+            return {
               byStart: byStart,
               byEnd: _(byStart).map(function(d, idx) {
                 return {end:d.end, idx:idx};
               }).sortBy('end').value()
-            });
+            };
           },
           function(e) { $log.log('TrackReader.load failed', name, e); throw e; }
-        );
+        ));
     };
 
     this.slice = function(opts) {
-      if (!_.has(self.tracks, opts.name)) {
+      if (!_.has(this.tracks, opts.name)) {
         $log.log('Missing', opts.name);
         throw new Err.TrackNotFound();
       }
-
-      var data = self.tracks[opts.name],
-          min = opts.startBase,
-          max = opts.endBase,
-          rightIdx = _.sortedIndex(data.byStart, {start: max}, 'start'),
-          leftPointerIdx = _.sortedIndex(data.byEnd, {end: min}, 'end'),
-          leftIdx = data.byStart.length -1;
-
-      while(leftPointerIdx < data.byEnd.length && data.byEnd[leftPointerIdx].end <= max){
-        leftIdx = Math.min(leftIdx, data.byEnd[leftPointerIdx].idx);
-        leftPointerIdx++;
-      }
-      return $q.when(data.byStart.slice(leftIdx, rightIdx));
+      return this.tracks[opts.name].then(function(data) {
+        var min = opts.startBase,
+            max = opts.endBase,
+            rightIdx = _.sortedIndex(data.byStart, {start: max}, 'start'),
+            leftPointerIdx = _.sortedIndex(data.byEnd, {end: min}, 'end'),
+            leftIdx = data.byStart.length -1;
+        while(leftPointerIdx < data.byEnd.length &&
+              data.byEnd[leftPointerIdx].end <= max) {
+          leftIdx = Math.min(leftIdx, data.byEnd[leftPointerIdx].idx);
+          leftPointerIdx++;
+        }
+        return data.byStart.slice(leftIdx, rightIdx);
+      });
     };
   })
   .factory('TrackReader', function(TrackReaderRTreeIndex, TrackReaderNaive, TrackBinarySearch) {
--- npactweb/npactweb/static/js/graphs/tests.js
+++ npactweb/npactweb/static/js/graphs/tests.js
@@ -261,66 +261,7 @@ describe('Graphs', function(){
   });
 
 
-  describe('TrackReader', function() {
-    var T,
-        extract = ['H-51*G complement(57104..57904)',
-                   'H-53*A complement(58013..59380)',
-                   'H-64-C 71945..72100',
-                   'G-125-G 88111..88275',
-                   'G-124*t 88544..88906',
-                   'H-102-C 112734..112931',
-                   'H-103*a 112864..113175',
-                   'H-115-a complement(129294..129599)',
-                   'H-116-G complement(131053..131163)']
-          .join('\n')
-    ;
-
-    beforeEach(inject(function(TrackReader){
-      T = TrackReader;
-    }));
-
-    describe('.load',function() {
-      it('loads', function() {
-        T.load('test', extract).then(function() {
-          expect(T.tracks.test).toBeDefined();
-        });
-        ng.$timeout.flush();
-      });
-    });
-
-    describe('.slice',function() {
-      beforeEach(function() {
-        T.load('test', extract);
-        ng.$timeout.flush();
-      });
-
-      it('slices', function() {
-        T.slice({name:'test', startBase:72000, endBase:88600})
-          .then(function(slice) {
-            expect(slice)
-              .toContain({start: 71945, end: 72100, complement: 0, name: 'H-64-C', phase: 0, approximate: false});
-            expect(slice)
-              .toContain({start: 88544, end: 88906, complement: 0, name: 'G-124*t', phase: 0, approximate: false});
-            expect(slice)
-              .toContain({start: 88111, end: 88275, complement: 0, name: 'G-125-G', phase: 2, approximate: false});
-          });
-        ng.$timeout.flush();
-      });
-      it('throws on bad names', function() {
-        expect(function() {
-          T.slice({name:'test2', startBase:0, endBase:0});
-        }).toThrow(new Err.TrackNotFound());
-      });
-      it('returns empty array on no matches', function() {
-        T.slice({name:'test', startBase:0, endBase:100})
-          .then(function(slice) {
-            expect(slice).toEqual([]);
-          });
-        ng.$timeout.flush();
-      });
-    });
-
-  });
+  ;
 
   describe('headerSpecCalc', function() {
     var testExtractsTrack = {text: 'test', lineType: 'extracts', active: true};
