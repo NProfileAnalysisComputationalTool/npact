NProfileAnalysisComputationalTool/npact

commit 2e9d25b2917b3444e63227f4458019ff20e2126e

Freed all Scodpot variables in Allplots.c

pynpact/src/Allplots.c
@@ -1616,7 +1616,9 @@ fprintf(stdout,"\n0 setlinejoin 0 setlinecap\n");
         /* Prints Hits */
         /***************/
 
-        if(nScp) fprintf(stdout,"\n");
+    if(nScp)
+    {
+    fprintf(stdout,"\n");
 
         for(i= 0; i < nScp; ++i)
 	{
@@ -1642,7 +1644,14 @@ fprintf(stdout,"\n0 setlinejoin 0 setlinecap\n");
                 fprintf(stdout, "L1 Gray");
                 fprintf(stdout, " %.1f %.2f M %.1f %.2f L stroke\n", (Scodpot[i][0] - (float)start) / delta * WIDTH, HIGHT + HIGHT_SCP, (Scodpot[i][1] - (float)start) / delta * WIDTH, HIGHT + HIGHT_SCP);
             }
+        free(Scodpot_type[i]);
+        free(Scodpot[i]);
         }
+    free(Scodpot_str);
+    free(Scodpot_col);
+    free(Scodpot_type);
+    free(Scodpot);
+    }
 
         fprintf(stdout,"\nL05\n");
 

commit 95d973ec00589918091a4394944ba7a5b0defb2f

Set pointers to NULL after free.
Since Allplots.c reuses pointers with realloc, and the first step of
realloc is to free the old pointer if it isn't null this was leading to
a double free bug and segfault. By setting them to NULL immediately
after free we avoid this bug introduced in [2e9d25b2].

re #946 (0.5)

pynpact/src/Allplots.c
@@ -1681,13 +1681,13 @@ fprintf(stdout,"\n0 setlinejoin 0 setlinecap\n");
                 fprintf(stdout, "L1 Gray");
                 fprintf(stdout, " %.1f %.2f M %.1f %.2f L stroke\n", (Scodpot[i][0] - (float)start) / delta * WIDTH, HIGHT + HIGHT_SCP, (Scodpot[i][1] - (float)start) / delta * WIDTH, HIGHT + HIGHT_SCP);
             }
-        free(Scodpot_type[i]);
-        free(Scodpot[i]);
+        free(Scodpot_type[i]); Scodpot_type[i] = NULL;
+        free(Scodpot[i]); Scodpot[i] = NULL;
         }
-    free(Scodpot_str);
-    free(Scodpot_col);
-    free(Scodpot_type);
-    free(Scodpot);
+    free(Scodpot_str); Scodpot_str = NULL;
+    free(Scodpot_col); Scodpot_col = NULL;
+    free(Scodpot_type); Scodpot_type = NULL;
+    free(Scodpot); Scodpot = NULL;
     }
 
         fprintf(stdout,"\nL05\n");