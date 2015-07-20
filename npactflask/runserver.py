e7eab72bdf1eeb488e0418c4692afcf548a5a180
Peter Sun <psun@acceleration.net>
2015-07-20 15:09:10 -0400

    Add Logging
    Logging wasn't working before. Now it can handle streamlogging

--- npactflask/runserver.py
+++ npactflask/runserver.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
+import logging
 from flask import Flask
 from npactflask import app
+
+logger = logging.getLogger('npactflask')
+logger.setLevel(logging.DEBUG)
+# logger.addHandler(logging.StreamHandler())
+fmt = "%(asctime)s %(levelname)-7s %(name)-20s| %(message)s"
+sh = logging.StreamHandler()
+sh.setFormatter(logging.Formatter(fmt, datefmt='%H:%M:%S'))
+logger.addHandler(sh)
+# logger.addHandler(logging.getLogger('werkzeug').handlers[0])
 if __name__ == '__main__':
     # Relevant documents:
     # http://werkzeug.pocoo.org/docs/middlewares/
@@ -13,4 +23,4 @@ if __name__ == '__main__':
     application = DispatcherMiddleware(Flask('dummy_app'), {
         app.config['APPLICATION_ROOT']: app,
     })
-    run_simple('localhost', 5000, application, use_reloader=True)
+    run_simple('localhost', 5000, application, use_reloader=True, use_debugger=True, use_evalex=True)
