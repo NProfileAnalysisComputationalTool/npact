8ce0c8103ab9b4d5c387f0559f22f30b0c113ace
Peter Sun <psun@acceleration.net>
2015-07-22 16:40:01 -0400

    Accession Number and Query Strings
    Accession Number search now works. Email and whatever was in the input
    now stays in the input if redirected back to the page.

--- npactflask/npactflask/views/start.py
+++ npactflask/npactflask/views/start.py
@@ -3,12 +3,10 @@ import logging
 import os.path
 import tempfile
 import urllib2
-from flask_wtf import Form
-from wtforms import fields
 from npactflask.views import settings, getrelpath
 from npactflask.views import is_clean_path, library_root
 from pynpact import util, entrez, parsing
-from flask import url_for, redirect, request, flash, send_from_directory
+from flask import url_for, redirect, request, flash
 
 
 logger = logging.getLogger(__name__)
@@ -27,20 +25,16 @@ def mksavefile(prefix):
     return (fd, abspath, relpath)
 
 
-def get_ti(size):
-    return fields.TextField(attrs={'size': size})
-
-
 def file_upload():
     fileup = request.files.get('file')
-
+    args = dict(request.args.items())
     if not fileup:
         flash('Must upload a File')
-        return redirect(url_for('start') + '?active=upload')
+        return redirect(url_for('start', **args))
     logger.info("Checking Uploaded file %s", fileup)
     if not is_clean_path(fileup.name):
         flash("Illegal filename")
-        return redirect(url_for('start') + '?active=upload')
+        return redirect(url_for('start', **args))
     fd, savepath, relpath = mksavefile("up-%s-" % fileup.filename)
     logger.info("Saving uploaded file to %r", savepath)
     fileup.save(savepath)
@@ -48,15 +42,16 @@ def file_upload():
 
 
 def fetchurl():
-    url = request.form.get('fetchurl')
+    args = dict(request.args.items())
+    url = args.pop('fetchurl', None)
     if not url:
         flash('URL Required in URL Field')
-        return redirect(url_for('start', active='url'))
+        return redirect(url_for('start', **args))
     logger.debug("Going to pull from %r", url)
     pull_req = urllib2.Request(url)
     if pull_req.get_type == "file":
         flash("Illegal URL")
-        return redirect(url_for('start') + '?active=url')
+        return redirect(url_for('start', **args))
     try:
         fh = urllib2.urlopen(pull_req)
         fd, savepath, relpath = mksavefile("url")
@@ -65,78 +60,71 @@ def fetchurl():
     except:
         logger.exception("Error fetching url %s", url)
         flash("Error fetching url")
-        return redirect(url_for('start') + '?active=url')
+        return redirect(url_for('start', **args))
 
 
 def pastein():
-    text = request.form.get('pastein')
-    # email = request.args.get['email']
+    args = dict(request.values.items())
+    text = args.pop('pastein', None)
+
     if not text:
         flash('Text Required in Pastefield')
-        return redirect(url_for('start') + '?active=pastein')
+        return redirect(url_for('start', **args))
     (fd, savepath, relpath) = mksavefile("txt")
     logger.info("Saving paste to %r", relpath)
     with os.fdopen(fd, 'wb') as fh:
         fh.write(text)
     flash('redirecting ' + relpath)
-    return redirect(url_for('run_frame', path=relpath))
+    return redirect(url_for('run_frame', path=relpath, **args))
 
 
 def search():
-    search = request.form.get('entrez_search_term')
+    args = dict(request.values.items())
+    search = args.pop('search', None)
     if not search:
         flash('Accession Number Required')
-        return redirect(url_for('start') + '?active=search')
-    self.active = 'entrez_search_term'
-    self.session = entrez.CachedEntrezSession(library_root())
+        return redirect(url_for('start', **args))
+    sess = entrez.CachedEntrezSession(library_root())
 
-    self.session.search(cleaned_data['entrez_search_term'])
+    sess.search(search)
     logger.debug(
         "Search finished, found %d matches",
-        self.session.result_count)
-    if self.session.result_count == 1:
-        cleaned_data['path'] = getrelpath(self.session.fetch())
-    elif self.session.result_count > 1:
-        raise Form.ValidationError(
+        sess.result_count)
+    if sess.result_count == 1:
+        relpath = getrelpath(sess.fetch())
+        return redirect(url_for('run_frame', path=relpath, **args))
+    elif sess.result_count > 1:
+        flash(
             "Too many results (%d) found, need 1."
             " Try refining the search or searching for a RefSeq id."
-            % (self.session.result_count))
+            % (sess.result_count))
+        return flask.render_template('start.html', sess=sess, search=search)
     else:
-        raise Form.ValidationError("No results found.")
+        flash("No results found.")
+        return redirect(url_for('start', **args))
 
 
 def view():
-    action = request.form.get('action')
-
+    active = request.form.get('active')
     if request.method == 'POST':
 
-        logger.info("Handling post action %r", action)
-        if action == 'pastein':
+        logger.info("Handling post active %r", active)
+        if active == 'pastein':
             return pastein()
-        elif action == 'fetchurl':
+        elif active == 'fetchurl':
             return fetchurl()
-        elif action == 'upload':
+        elif active == 'upload':
             return file_upload()
-    #     if form.is_valid():
-
-    #         logger.info("Form is valid; action is %r", action)
-    #         kwargs = startform.cleaned_data
-    #         path = kwargs.pop('path')
-    #         if action == 'run':
-    #             return redirect(url_for(action, path=path) + kwargs)
-    #         else:
-    #             logger.error("Unknown action %r", action)
-    #             flash("Unknown action.")
-    # else:
-    #     email = startform['email'].data or request.args.get('email')
-    return flask.render_template(
-        'start.html', **{
-            'action': action
-        })
+        elif active == 'search':
+            return search()
+    else:
+        # not a post
+        args = dict(request.args.items())
+        return flask.render_template('start.html', **args)
 
 
 def re_search():
-    if request.REQUEST.get('entrez_search_term'):
+    if request.args.get('entrez_search_term'):
         return flask.render_template(
             'start.html')
     else:
@@ -154,16 +142,8 @@ def efetch(id):
         flash("There was a problem loading file '%s', "
               "please try again or try a different record."
               % path)
-        return re_search(request)
+        return re_search()
 
-    path = getrelpath(abspath)
-    remaining_args = dict(request.REQUEST.items())
-    action = remaining_args.pop('action', 'run')
+    args = dict(request.args.items())
 
-    if action in ['run', 'config']:
-        return redirect(
-            url_for(action, path=path) + remaining_args)
-    else:
-        logger.error("Unknown action %r", action)
-        flash("Unknown action.")
-        return re_search(request)
+    return redirect(url_for('run_frame', path=path, **args))
