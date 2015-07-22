416e8bf2a91382d5152762369381d2fde43564fb
Nathan Bird <nathan@acceleration.net>
2015-07-22 17:26:07 -0400

    redirect to /npact/ from /

--- npactflask/runserver.py
+++ npactflask/runserver.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import logging
-from flask import Flask
+from flask import Flask, redirect
 from npactflask import app
 
 logger = logging.getLogger('npactflask')
@@ -11,6 +11,16 @@ sh = logging.StreamHandler()
 sh.setFormatter(logging.Formatter(fmt, datefmt='%H:%M:%S'))
 logger.addHandler(sh)
 # logger.addHandler(logging.getLogger('werkzeug').handlers[0])
+
+
+redirectapp = Flask('redirectapp')
+
+
+@redirectapp.route('/')
+def doredirect():
+    return redirect(app.config['APPLICATION_ROOT'])
+
+
 if __name__ == '__main__':
     # Relevant documents:
     # http://werkzeug.pocoo.org/docs/middlewares/
@@ -18,9 +28,10 @@ if __name__ == '__main__':
     from werkzeug.serving import run_simple
     from werkzeug.wsgi import DispatcherMiddleware
     app.config['DEBUG'] = True
-    # Load a dummy app at the root URL to give 404 errors.
+    # Load a redirect app at the root URL to redirect us to the target app.
     # Serve app at APPLICATION_ROOT for localhost development.
-    application = DispatcherMiddleware(Flask('dummy_app'), {
+    application = DispatcherMiddleware(redirectapp, {
         app.config['APPLICATION_ROOT']: app,
     })
-    run_simple('localhost', 5000, application, use_reloader=True, use_debugger=True, use_evalex=True)
+    run_simple('localhost', 5000, application,
+               use_reloader=True, use_debugger=True, use_evalex=True)
