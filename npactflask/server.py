883ee4a748edcaf58c9f8dd876a3a6c95c9282e2
Nathan Bird <nathan@acceleration.net>
2015-07-28 14:28:03 -0400

    Work on the dev/live server modes
    Trying to improve the dev and live server startup commands and logging
    while using gunicorn + gevent
    
    re #992 (2.5)

---
+++ npactflask/server.py
@@ -0,0 +1,24 @@
+from gevent.monkey import patch_all
+patch_all(subprocess=True)
+
+from npactflask import app, app_with_redirect
+application = app_with_redirect
+
+
+import os
+import logging
+from logging.handlers import WatchedFileHandler
+
+logger = logging.getLogger('')
+logger.setLevel(logging.DEBUG)
+fmt = "%(asctime)s %(levelname)-7s %(name)-20s| %(message)s"
+fh = WatchedFileHandler(app.config['WEBROOT'] / 'logs/main.log')
+fh.setFormatter(logging.Formatter(fmt))
+logger.addHandler(fh)
+
+if os.environ.get('ENV', 'dev').lower() in ('dev', 'development'):
+    # Causes duplicate logging
+    #app.config['DEBUG'] = True
+    sh = logging.StreamHandler()
+    sh.setFormatter(logging.Formatter(fmt, datefmt='%H:%M:%S'))
+    logger.addHandler(sh)
