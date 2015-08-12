67805468b358ea883092e45d8ffc27913ade9f46
Peter Sun <psun@acceleration.net>
2015-08-12 13:46:00 -0400

    Remove PDF Modal
    PDF Modal was still getting referenced when it no longer existed

--- npactflask/npactflask/static/js/graphs/page.js
+++ npactflask/npactflask/static/js/graphs/page.js
@@ -53,7 +53,7 @@ angular.module('npact')
     };
   })
 
-  .controller('DownloadsCtrl', function($scope, $log, PredictionManager, PDFModal, MessageBus, Pynpact, StatusPoller, GraphConfig, dialogService) {
+  .controller('DownloadsCtrl', function($scope, $log, PredictionManager, MessageBus, Pynpact, StatusPoller, GraphConfig, dialogService) {
     'use strict';
     $scope.$watch( function() { return PredictionManager.files; },
                    function(val) { $scope.predictionFiles = val; });
